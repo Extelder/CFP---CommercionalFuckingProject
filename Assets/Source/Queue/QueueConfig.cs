b88e8eec74bcb0636c5f440dbfb89c4900c1fa1e
igoryava <126491702+igoryava@users.noreply.github.com>
2026-01-15 01:35:22 +0700

    added abstract factory base for units AND QUEUE

---
+++ Assets/Source/Queue/QueueConfig.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QueueConfig", menuName = "Queue")]
+public class QueueConfig : ScriptableObject
+{
+    [field: SerializeField] public int MaxQueueCapacity { get; private set; }
+}
